72522c685f64b666a118f8a22d00df83812f56ce
Hugh Sorby <h.sorby@auckland.ac.nz>
2014-10-18 10:54:15 +1300

    Add content to version api header file.

---
+++ src/api/yawot/version.h
@@ -0,0 +1,16 @@
+
+
+namespace yawot
+{
+
+/**
+ * Return the version of the *library* as an
+ * unsigned integer.  The format is XXYYZZ
+ * XX - major version
+ * YY - minor version
+ * ZZ - patch version
+ */
+unsigned int version();
+
+}
+
